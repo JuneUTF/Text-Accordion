48fcea6d299ab2ce94c39a3608a4e3046697beb3
PHAN VAN HUYNH <94588311+JuneUTF@users.noreply.github.com>
2022-04-19 19:54:23 +0900

    add style animation text show
    juneutf

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ const AccordionBtn = document.querySelectorAll('.accordion')
 AccordionBtn.forEach((el) => {
     el.addEventListener('click', (e) => {
         el.querySelector('.icondown').classList.toggle('active')
-        el.querySelector('.accordion-text').classList.toggle('isshow')
+        const ani = el.querySelector('.accordion-text');
+        ani.classList.toggle('isshow')
+        if (!ani.classList.contains('isshow')) { ani.style.height = 0; } else {
+            ani.style.height = `${ani.scrollHeight}px`
+        }
     })
 })
